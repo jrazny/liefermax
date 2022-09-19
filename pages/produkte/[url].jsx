4decf6c8d34d50cb3c2a110163c6e6568c3fb394
Jan Razny <netorg@gmx.de>
2022-09-19 18:11:46 +0200

    mongodb

--- pages/produkte/[url].jsx
+++ pages/produkte/[url].jsx
@@ -1,22 +1,68 @@
 import { useRouter } from "next/router";
 import jsondb from "../../jsondb/produkte";
+import Link from "next/link";
+import Image from "next/Image";
+import { ListGroup, Button, ListGroupItem } from "react-bootstrap";
 
 export default function Produktseite() {
-    const router = useRouter();
-    const {url} = router.query;
-    const produkt = jsondb.produkte.find((a) => a.url === url)
+  const router = useRouter();
+  const { url } = router.query;
+  const produkt = jsondb.produkte.find((a) => a.url === url);
 
-    if (!produkt) {
-        return (
-            <div>
-                <h2>
-                    Produkt nicht vorhanden
-                </h2>
-            </div>
-        )
-    }
-
+  if (!produkt) {
     return (
-        <div>{produkt.beschreibung}</div>
-    )
+      <div>
+        <h2>Produkt nicht vorhanden</h2>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <div>
+        <Link href="/">
+          <a className="text-dark">← zurück zur Übersicht</a>
+        </Link>
+      </div>
+
+      <div className="row row-cols-2 mt-2">
+        <div>
+          <Image
+            className="rounded-3"
+            src={produkt.bild}
+            alt={produkt.name}
+            width={600}
+            height={600}
+            layout="responsive"
+          />
+        </div>
+        <div>
+          <h1>{produkt.name}</h1>
+          <ListGroup variant="flush">
+            <ListGroupItem>
+              <h2 className="text-danger">{produkt.preis}</h2>
+            </ListGroupItem>
+            <ListGroupItem>{produkt.beschreibung}</ListGroupItem>
+            <ListGroupItem>
+              Extras: doppelt <input className="form-check-input me-2" type="checkbox" />
+              extra Pommes <input className="form-check-input me-2" type="checkbox" />
+            </ListGroupItem>
+            <ListGroupItem>
+              <input
+                className="form-control w50"
+                type="number"
+                placeholder="1"
+                min="1"
+              ></input>
+            </ListGroupItem>
+            <ListGroupItem>
+              <div className="row shadow">
+                <Button variant="danger">zum Warenkorb</Button>
+              </div>
+            </ListGroupItem>
+          </ListGroup>
+        </div>
+      </div>
+    </div>
+  );
 }
--- pages/warenkorb.jsx
+++ pages/warenkorb.jsx
@@ -1,7 +1,62 @@
-export default function warenkorb() {
+import { Table, CloseButton, Button, Card } from "react-bootstrap"
+import Image from "next/image"
+import CardHeader from "react-bootstrap/esm/CardHeader";
+
+export default function Warenkorb() {
     return (
       <div>
         <h1>Warenkorb</h1>
+        <div className="row mt-4">
+          <div className="col-9">
+            <Table hover responsive>
+              <thead>
+                <tr>
+                  <th>Bild</th>
+                  <th>Name</th>
+                  <th>Extras</th>
+                  <th>Menge</th>
+                  <th>Betrag</th>
+                  <th><CloseButton disabled /></th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>
+                    <Image src={'/images/produkte/cola.jpg'} alt="Cola" width={50} height={50} />
+                  </td>
+                  <td>Cola</td>
+                  <td>doppelt</td>
+                  <td>1</td>
+                  <td>1,99</td>
+                  <td><Button className="btn-sm">x</Button></td>
+                </tr>
+                <tr>
+                  <td>
+                    <Image src={'/images/produkte/pommes.jpg'} alt="Pommes" width={50} height={50} />
+                  </td>
+                  <td>Pommes</td>
+                  <td>doppelt</td>
+                  <td>1</td>
+                  <td>3,50</td>
+                  <td><Button className="btn-sm">x</Button></td>
+                </tr>
+              </tbody>
+            </Table>
+          </div>
+          <div className="col-3 p-2">
+            <div className="shadow">
+              <Card>
+                <Card.Header as="h5">Gesamt</Card.Header>
+                <Card.Body className="text-center">
+                  <Card.Title>
+                    6,95 EUR
+                  </Card.Title>
+                  <Button variant="primary">zur Kasse</Button>
+                </Card.Body>
+              </Card>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
